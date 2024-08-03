965eeaa1d7caf0ade1d07693df9e29dd319c451d
semilemons <mr.semilemons@gmail.com>
2024-08-03 20:17:50 +0900

    b

--- src/app/staff/page.jsx
+++ src/app/staff/page.jsx
@@ -1,5 +1,5 @@
 // app/staff/page.js
-import StaffDashboard from '../components/StaffDashboard';
+import StaffDashboard from '../../components/StaffDashboard';
 
 export default function StaffPage() {
   return <StaffDashboard />;
