d031876ae7b228d8467219bc6960caf3169e1b18
semilemons <mr.semilemons@gmail.com>
2024-08-03 19:58:54 +0900

    アップデート

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-// layout.js
+// app/layout.js
 import { Inter } from "next/font/google";
 import Script from 'next/script'
 import "./globals.css";
@@ -13,24 +13,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ja">
-      <head>
-      <Script
-  id="google-analytics"
-  src="https://www.googletagmanager.com/gtag/js?id=G-WTK6KTMQ7Z"
-  strategy="afterInteractive"
-/>
-<Script id="google-analytics" strategy="afterInteractive">
-  {`
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-WTK6KTMQ7Z');
-  `}
-</Script>
-
-      </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-WTK6KTMQ7Z"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-WTK6KTMQ7Z');
+          `}
+        </Script>
+      </body>
     </html>
   );
 }
