297ca5a9aed668e8e4bbc73e0d78ca722349678d
semilemons <mr.semilemons@gmail.com>
2024-08-03 14:52:59 +0900

    ga4 修正

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+// layout.js
 import { Inter } from "next/font/google";
 import Script from 'next/script'
 import "./globals.css";
@@ -13,22 +14,21 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ja">
       <head>
-        <Script
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=G-F44KQXWZ47`}
-        />
-        <Script
-          id="google-analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-F44KQXWZ47');
-            `,
-          }}
-        />
+      <Script
+  id="google-analytics"
+  src="https://www.googletagmanager.com/gtag/js?id=G-WTK6KTMQ7Z"
+  strategy="afterInteractive"
+/>
+<Script id="google-analytics" strategy="afterInteractive">
+  {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-WTK6KTMQ7Z');
+  `}
+</Script>
+
       </head>
       <body className={inter.className}>{children}</body>
     </html>
